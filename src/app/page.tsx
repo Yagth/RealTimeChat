ca885f2775d402bbc6408eed141d8d6a9e45448b
Yab <yabuman46@gmail.com>
2023-09-04 11:33:20 +0300

    Finished setting up database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Button from "@/components/ui/Button";
+import { db } from "@/lib/db";
 
-export default function Home() {
-  return <Button />;
+export default async function Home() {
+  return <Button variant={"ghost"}>Hello world</Button>;
 }
