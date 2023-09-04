7c0c65b82d6454985f2d2bdf0ac94f670f834aba
Yab <yabuman46@gmail.com>
2023-09-04 12:01:15 +0300

    Next auth configuration

---
+++ src/types/db.d.ts
@@ -0,0 +1,7 @@
+// *.d.ts means that we don't have to import them through out our application it is available by default. (It means definition)
+interface User {
+  name: string;
+  email: string;
+  image: string;
+  id: string;
+}
